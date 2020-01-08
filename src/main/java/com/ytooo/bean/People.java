c5606aba5b8fa2f778ac8dcce594b6843e28b1ba
youminda <you.minda@ebiz-fooge.com>
2020-01-08 10:11:49 +0800

    ytooo

--- src/main/java/com/ytooo/bean/People.java
+++ src/main/java/com/ytooo/bean/People.java
@@ -11,9 +11,12 @@ public class People {
 
     private String name;
 
-    public People(int sex, String name) {
+    private String drlType;
+
+    public People(int sex, String name, String drlType) {
         this.sex = sex;
         this.name = name;
+        this.drlType = drlType;
     }
 
     public People() {
--- src/test/java/com/ytooo/DroolsApplicationTests.java
+++ src/test/java/com/ytooo/DroolsApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -26,6 +27,7 @@ class DroolsApplicationTests {
         People people = new People();
         people.setName("达");
         people.setSex(1);
+        people.setDrlType("people");
         session.insert(people);//插入
         session.fireAllRules();//执行规则
     }
@@ -39,15 +41,26 @@ class DroolsApplicationTests {
     }
     @Test
     public void from() {
-        People p1 = new People(1,"达");
-        People p2 = new People(0,"秋");
-        People p3 = new People(3,"金");
+        People p1 = new People(1,"达","from");
+        People p2 = new People(0,"秋","from");
+        People p3 = new People(3,"金","from");
         Animal animal = new Animal();
         animal.setPeoples(new ArrayList<>());
         animal.getPeoples().add(p1);
         animal.getPeoples().add(p2);
         animal.getPeoples().add(p3);
         session.insert(animal);//插入
+        session.fireAllRules();//执行规则
+    }
+   @Test
+    public void collect() {
+       session.insert(new People(1, "达","collect"));
+       session.insert(new People(0, "秋","collect"));
+       session.insert(new People(0, "春","collect"));
+       session.insert(new People(1, "夏","collect"));
+       session.insert(new People(0, "冬","collect"));
+       session.insert(new People(3, "金","collect"));
+
         session.fireAllRules();//执行规则
     }
 
